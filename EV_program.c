9c5fdf501db7488bfd42d983d6ced713ba138d85
nikhil52cs <119278011+nikhil52cs@users.noreply.github.com>
2022-11-28 10:23:48 +0530

    Update EV_program.c

--- EV_program.c
+++ EV_program.c
@@ -5,6 +5,7 @@ int main()
 {
   
     // prints hello world
+    printf("I am updating my file.");
     printf("Hello World , The future is electric and Euler wants India to steer it.");
   
     return 0;
