0bcc3b178b82bb968ed8859485e4bbba9553950f
nikhil52cs <119278011+nikhil52cs@users.noreply.github.com>
2022-11-28 16:04:42 +0530

    Update EV_program.c

--- EV_program.c
+++ EV_program.c
@@ -5,7 +5,7 @@ int main()
 {
   
     // prints hello world
-    printf("I am updating my file upload and download artifact from Jfrog.");
+    
     printf("Hello World , The future is electric and Euler wants India to steer it.");
   
     return 0;
